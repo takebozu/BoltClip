876cc89803aebb4b8ef535b9372e257372c8b087
Satoshi Takezawa <stakezawa@terrasky.co.jp>
2026-03-31 15:51:55 +0900

    Adapt to light/dark mode

--- Clipy/Sources/Preferences/CPYPreferencesWindowController.swift
+++ Clipy/Sources/Preferences/CPYPreferencesWindowController.swift
@@ -54,10 +54,6 @@ final class CPYPreferencesWindowController: NSWindowController {
     override func windowDidLoad() {
         super.windowDidLoad()
         self.window?.collectionBehavior = .canJoinAllSpaces
-        self.window?.backgroundColor = NSColor(white: 0.99, alpha: 1)
-        if #available(OSX 10.10, *) {
-            self.window?.titlebarAppearsTransparent = true
-        }
         toolBarItemTapped(generalButton)
         generalButton.sendAction(on: .leftMouseDown)
         menuButton.sendAction(on: .leftMouseDown)
